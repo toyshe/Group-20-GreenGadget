eb3350da58ab239f19fdcabc0296b224b10c1faa
toyshe <toyshehossain@outlook.com>
2024-04-04 19:30:04 +0100

    adding get electronic by id and patch electronic by quantity

--- Backend/controllers/electronics.controller.js
+++ Backend/controllers/electronics.controller.js
@@ -1,34 +1,71 @@
-const { findElectronics, insertElectronics } = require("../models/electronics.model")
-const { checkCategoryExists, checkShopkeeperExists } = require("../utils/check-exists")
+const {
+  findElectronics,
+  insertElectronics,
+  updateElectronicsById,
+  findElectronicById,
+} = require("../models/electronics.model");
+const {
+  checkCategoryExists,
+  checkShopkeeperExists,
+} = require("../utils/check-exists");
 
 exports.getElectronics = (req, res, next) => {
-    const {electronics_type, sort_by, order, shopkeeper} = req.query
+  const { electronics_type, sort_by, order, shopkeeper } = req.query;
 
-    const fetchQuery = findElectronics(electronics_type, sort_by, order, shopkeeper)
-    const queries = [fetchQuery]
+  const fetchQuery = findElectronics(
+    electronics_type,
+    sort_by,
+    order,
+    shopkeeper
+  );
+  const queries = [fetchQuery];
 
-    if(electronics_type){
-        const categoriesExistenceQuery = checkCategoryExists(electronics_type)
-        queries.push(categoriesExistenceQuery)
-    }
+  if (electronics_type) {
+    const categoriesExistenceQuery = checkCategoryExists(electronics_type);
+    queries.push(categoriesExistenceQuery);
+  }
 
-    if(shopkeeper){
-        const shopkeeperExistenceQuery = checkShopkeeperExists(shopkeeper)
-        queries.push(shopkeeperExistenceQuery)
-    }
+  if (shopkeeper) {
+    const shopkeeperExistenceQuery = checkShopkeeperExists(shopkeeper);
+    queries.push(shopkeeperExistenceQuery);
+  }
 
-    Promise.all(queries).then(([fetchQuery]) => {
-        res.status(200).send({electronics: fetchQuery})
-    }).catch((err) => {
-        next(err)
+  Promise.all(queries)
+    .then(([fetchQuery]) => {
+      res.status(200).send({ electronics: fetchQuery });
     })
-}
+    .catch((err) => {
+      next(err);
+    });
+};
 
 exports.postElectronics = (req, res, next) => {
-    const electronic = req.body;
-    insertElectronics(electronic).then((electronics) => {
-        res.status(201).send({electronics})
-    }).catch((err) => {
-        next(err)
+  const electronic = req.body;
+  insertElectronics(electronic)
+    .then((electronics) => {
+      res.status(201).send({ electronics });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getElectronicById = (req, res, next) => {
+    const {electronics_id} = req.params;
+    findElectronicById(electronics_id).then((electronic) => {
+        res.status(200).send({ electronic });
     })
 }
+
+exports.patchElectronicsById = (req, res, next) => {
+  const { electronics_id } = req.params;
+  const { updatedQuantity } = req.body;
+
+  updateElectronicsById(electronics_id, updatedQuantity)
+    .then((electronic) => {
+      res.status(200).send({ electronic });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
