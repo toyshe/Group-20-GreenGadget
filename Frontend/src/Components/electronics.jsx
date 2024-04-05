b105c42d10e8cbccc771cc7461d24b8d3503ce8a
dane679 <danpelicue@gmail.com>
2024-04-05 20:10:06 +0100

    make requests to the be using axios

--- Frontend/src/Components/electronics.jsx
+++ Frontend/src/Components/electronics.jsx
@@ -1,1 +1,24 @@
-import axios from 'axios'; 
+import getAllElecronics from "../util/utils"; 
+import { useEffect, useState } from 'react';
+import { getElectronics } from '../../../Backend/controllers/electronics.controller';
+//import {customErros} from '../import ErrorHandling from './ErrorHandling';
+
+export default function Electronics({ electronics, setElectronics}){
+
+    //getElectronics
+
+    const handleElectronicsClick = (electronic) =>{
+        navigate(`/electronics/${electronic_id}`)
+    };
+
+    if (error){
+        return 
+    }
+
+    /*return(
+        <div className="electronics">
+        <
+
+    );
+    add styling for to app.css*/
+}
---
+++ Frontend/utilis/utilis.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+const electronicsApi = axios.create({
+    baseURL:'https://group-20-greengadget.onrender.com'
+});
+
+export default function getAllElecronics(categories, sortBy, order, page ){
+    if(!sortBy){
+        sortBy = "Price";
+    }
+    if(!order){
+        order="asc";
+    }
+    return electronicsApi
+        .get(`/electronics`, {params: {categories, sort_by: sortBy, order, p:page }})
+        .then((response) => {
+            return response.data;
+        })
+        .catch((err) => {
+            throw err;
+        });
+}
+
+export function getElectronicsById(electronics_id){
+    return electronicsApi.get(`/electronics/${electronics_id}`).then((response) => {
+        return response.data;
+    }).catch(err=> {
+        throw err;
+    })
+}
+
+
+export function displayCategories() {
+    return electronicsApi
+    .get(`/categories`)
+    .then((response) => {
+        return response.data;
+    })
+    .catch((err) => {
+        throw err;
+    });
+}
