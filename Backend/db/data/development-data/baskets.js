bd5fa14b89cb1a136e1445770fed56ac6f980c93
Fahad4569 <alfahas2@lsbu.ac.uk>
2024-04-20 12:58:30 +0100

    basket data update

---
+++ Backend/db/data/development-data/baskets.js
@@ -0,0 +1,126 @@
+module.exports = [
+    {
+        username: 'jsmith21'
+        electronics_id: 1,
+        quantity: 2,
+        created_at: '2024-01-25'
+    },
+    {
+        username: 'emilyjohnson'
+        electronics_id: 2,
+        quantity: 1,
+        created_at: '2024-02-25'
+    },
+    {
+        username: 'jemmab756'
+        electronics_id: 3,
+        quantity: 2,
+        created_at: '2024-02-10'
+    },
+    {
+        username: 'mw456216'
+        electronics_id: 4,
+        quantity: 3,
+        created_at: '2024-01-01'
+    },
+    {
+        username: 'staylor12'
+        electronics_id: 5,
+        quantity: 1,
+        created_at: '2024-03-01'
+    },   
+    {
+        username: 'davwies145'
+        electronics_id: 6,
+        quantity: 2,
+        created_at: '2024-01-29'
+    },
+    {
+        username: 'olivia7evans'
+        electronics_id: 7,
+        quantity: 1,
+        created_at: '2024-02-05'
+    },
+    {
+        username: 'daniel70456'
+        electronics_id: 8,
+        quantity: 2,
+        created_at: '2024-02-16'
+    },
+    {
+        username: 'isabellaz74'
+        electronics_id: 9,
+        quantity: 4,
+        created_at: '2024-02-01'
+    },
+    {
+        username: 'cartere756'
+        electronics_id: 10,
+        quantity: 1,
+        created_at: '2024-03-21'
+    },
+    {
+        username: 'jsmith21'
+        electronics_id: 5,
+        quantity: 2,
+        created_at: '2024-01-25'
+    },
+    {
+        username: 'emilyjohnson'
+        electronics_id: 7,
+        quantity: 1,
+        created_at: '2024-02-25'
+    },
+    {
+        username: 'jemmab756'
+        electronics_id: 4,
+        quantity: 1,
+        created_at: '2024-02-10'
+    },
+    {
+        username: 'mw456216'
+        electronics_id: 9,
+        quantity: 2,
+        created_at: '2024-01-01'
+    },
+    {
+        username: 'staylor12'
+        electronics_id: 1,
+        quantity: 1,
+        created_at: '2024-03-01'
+    },   
+    {
+        username: 'davwies145'
+        electronics_id: 8,
+        quantity: 1,
+        created_at: '2024-01-29'
+    },
+    {
+        username: 'olivia7evans'
+        electronics_id: 3,
+        quantity: 1,
+        created_at: '2024-02-05'
+    },
+    {
+        username: 'daniel70456'
+        electronics_id: 2,
+        quantity: 2,
+        created_at: '2024-02-16'
+    },
+    {
+        username: 'isabellaz74'
+        electronics_id: 5,
+        quantity: 1,
+        created_at: '2024-02-01'
+    },
+    {
+        username: 'cartere756'
+        electronics_id: 6,
+        quantity: 3,
+        created_at: '2024-03-21'
+    }   
+   
+
+
+
+]
