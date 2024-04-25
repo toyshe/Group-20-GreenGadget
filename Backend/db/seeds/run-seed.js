5457faadb688e26e7023aeda35fbb9c3f3c8719e
toyshe <toyshehossain@outlook.com>
2024-04-25 22:24:51 +0100

    changing data back to dev data

--- Backend/db/seeds/run-seed.js
+++ Backend/db/seeds/run-seed.js
@@ -1,10 +1,9 @@
 const devData = require("../data/development-data/index.js")
-const testData = require('../data/test-data/index.js')
 const seed = require("./seed.js");
 const db = require("../connection.js");
 
 const runSeed = () => {
-  return seed(testData).then(() => db.end());
+  return seed(devData).then(() => db.end());
 };
 
 runSeed();
