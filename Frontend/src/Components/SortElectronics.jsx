ecb12e727d48aff3c2a36c248eb9da9b3f0011b3
toyshe <toyshehossain@outlook.com>
2024-06-02 11:31:47 +0100

    adding styles for filters for electronics

--- Frontend/src/Components/SortElectronics.jsx
+++ Frontend/src/Components/SortElectronics.jsx
@@ -1,29 +1,36 @@
 export default function SortElectronics({ setSortBy, setOrder }) {
-    const handleSortChange = (event) => {
-      setSortBy(event.target.value);
-    };
-    const handleOrderChange = (event) => {
-      setOrder(event.target.value);
-    };
-    return (
+  const handleSortChange = (event) => {
+    setSortBy(event.target.value);
+  };
+  const handleOrderChange = (event) => {
+    setOrder(event.target.value);
+  };
+  return (
     <>
-      <p>Sort By</p>
-      <select onChange={handleSortChange}>
-        <optgroup label="choose a filter">
-        {/* <option value="created_at">Date</option> */}
-        <option value="price">Price</option>
-        <option value="storage_in_gb">storage</option>
-        </optgroup>
-      </select>
+      <div className="filters-container">
+        <div className="filter-group">
+          <p>Sort By: </p>
+          <div className="dropdown-content">
+            <select onChange={handleSortChange} defaultValue=''>
+              <option value='' disabled>Select a filter</option>
+              <option value="price">Price</option>
+              <option value="storage_in_gb">storage</option>
+            </select>
+          </div>
+        </div>
 
-      <p>Order</p>
-      <select onChange={handleOrderChange}>
-        <optgroup label="choose how you want devices ordered">
-        <option value="asc">Ascending</option>
-        <option value="desc">Descending</option>
-        </optgroup>
-      </select>
+        <div className="filter-group">
+          <p>Order: </p>
+          <div className="dropdown-content">
+            <select onChange={handleOrderChange} defaultValue=''>
+              <option value='' disabled>Select order</option>
+              <option value="asc">Ascending</option>
+              <option value="desc">Descending</option>
+            </select>
+          </div>
+        </div>
+      </div>
     </>
 
-    );
+  );
 }
