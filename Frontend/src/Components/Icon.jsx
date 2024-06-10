199a4468958189c8079ece7e9897931ea5b8d793
dane679 <danpelicue@gmail.com>
2024-06-10 22:55:45 +0100

    icons can now navigate to the associated item category.

--- Frontend/src/Components/Icon.jsx
+++ Frontend/src/Components/Icon.jsx
@@ -3,14 +3,22 @@ import { MdTabletMac } from "react-icons/md";
 import { BsSmartwatch } from "react-icons/bs";
 import { IoPhonePortraitSharp } from "react-icons/io5";
 import { AiOutlineLaptop } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 const Icon = ({props, className = "", size}) => {
     const Icontype = props === "Phone" ? IoPhonePortraitSharp 
     : props === 'Laptop' ?  AiOutlineLaptop
     : props === "Smartwatch" ? BsSmartwatch
     : MdTabletMac;
+
+    const navigate = useNavigate()
+    
+    const Icononclick = props === "Phone" ? (()=>{navigate(`/electronics?electronics_type=${props}`)})
+    : props === 'Laptop' ?  (()=>{navigate(`/electronics?electronics_type=${props}`)})
+    : props === "Smartwatch" ? (()=>{navigate(`/electronics?electronics_type=${props}`)})
+    : (()=>{navigate(`/electronics?electronics_type=${props}`)});
   return (
-    <Icontype size={size} className={className}/>
+    <Icontype size={size} className={className} onClick={Icononclick}/>
   )
 }
 
