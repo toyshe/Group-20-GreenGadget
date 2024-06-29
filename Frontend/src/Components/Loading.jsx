e60b9c8157af6f5bc07b57bf372a2ed7811b712d
dane679 <danpelicue@gmail.com>
2024-06-29 19:38:56 +0100

    animated ellipses

--- Frontend/src/Components/Loading.jsx
+++ Frontend/src/Components/Loading.jsx
@@ -3,27 +3,14 @@ import './loading.css';
 import favicon from"/favicon.png";
 
 export default function Loading({loadingHeader}){
-    let dots = `.`;
-    let x = 0 ;
-    useEffect(()=>{     
-            setTimeout(() => {
-                
-                if(x < 6){
-                    x += 1;
-                }
-            }, 1000);
-    },[x]);
-    useEffect(()=>{
-        const loadhead = document.getElementById("load-heading");
-    
-            console.log(dots.repeat(x))
-            loadhead.innerText = `Loading ${loadingHeader ? loadingHeader : ""} please wait${dots.repeat(x)}`;
-           
-        },[x]);
 
     return (
         <div className="loading-page">
-            <h2 id='load-heading'>Loading {loadingHeader ? loadingHeader : null} please wait</h2>
+            <h2 id='load-heading'>Loading {loadingHeader ? loadingHeader : null} please wait
+                <span className='ellipsis'>.</span>
+                <span className='ellipsis'>.</span>
+                <span className='ellipsis'>.</span>
+            </h2>
             <div className="loading-animation-container">
             <div className='cube-pos'>
                 <div className='loadbox1'><img src= {favicon} alt="placeholder img1"></img></div>
