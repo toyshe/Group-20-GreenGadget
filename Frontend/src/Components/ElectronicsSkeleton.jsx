70d61a678f5c50ddc14f03792c31fc9d38d4f918
dane679 <danpelicue@gmail.com>
2024-08-31 01:09:30 +0100

    updates

--- Frontend/src/Components/ElectronicsSkeleton.jsx
+++ Frontend/src/Components/ElectronicsSkeleton.jsx
@@ -19,6 +19,11 @@ export default function ElectronicsSkeleton() {
             <li><ElectronicsSkeletonbutton /></li>
             <li><ElectronicsSkeletonbutton /></li>
             <li><ElectronicsSkeletonbutton /></li>
+            <div className="pagination-buttons">
+              <button>1</button>
+              <button>1</button>
+              <button>1</button>
+            </div>
         </ul>
         <Totop />
     </div>
